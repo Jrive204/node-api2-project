d9d504c60e8d1228b316e38be4c977fbd79bacce
Jose <Jrivera6869@yahoo.com>
2020-02-13 15:16:27 -0500

    dep rdy

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const server = express();
 const postsRouter = require("./Routes/posts-router");
@@ -9,7 +11,7 @@ server.use(cors());
 
 server.use("/api/posts", postsRouter);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on: ${port} ***\n`);
 });
